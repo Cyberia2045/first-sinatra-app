407d396add1eb635d66d2b456215353f957aca34
Cyberia 2045 <sagansociety86@gmail.com>
2017-05-11 15:20:27 -0400

    created a simple sign_in with Sinatra

--- app.rb
+++ app.rb
@@ -14,5 +14,35 @@ get '/contact' do
 end
 
 get '/posts' do
-	
+	@posts = ["post1", "post2", "post3"]
+	erb :posts
 end
+
+get '/search' do
+	@search_term = params[:search]
+	p params
+	erb :search
+end
+
+get '/sign_in' do
+	@email = params[:email]
+	@password = params[:password]
+	erb :sign_in
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
